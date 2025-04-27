479748a1b65e97ca85ebc8a374f1e7ff54121083
raunogroch <raul.noguera.rocha@gmail.com>
2025-04-26 20:11:33 -0400

    agregando archivo helper y corrigiendo error de formato al momento de guardar en la base de datos

--- src/controller/controlador_registrar_asistenacia.php
+++ src/controller/controlador_registrar_asistenacia.php
@@ -1,60 +1,60 @@
 <?php
 if (!empty($_POST["btnentrada"])) {
     if (!empty($_POST["txtCI"])) {
-        $CI=$_POST["txtCI"];
-        $consulta=$conn->query( "SELECT COUNT(*) as 'total' from empleado where CI='$CI' ");
-        $id=$conn->query( "SELECT id_empleado from empleado where CI='$CI' ");
+        $CI = $_POST["txtCI"];
+        $consulta = $conn->query("SELECT COUNT(*) as 'total' from empleado where CI='$CI' ");
+        $id = $conn->query("SELECT id_empleado from empleado where CI='$CI' ");
         if ($consulta->fetch_object()->total > 0) {
-            
-            $fecha=date("Y-m-d h:i:s");
-            $id_empleado=$id->fetch_object()->id_empleado;
 
-            $consultafecha=$conn->query(" SELECT entrada  from asistencia WHERE id_empleado=$id_empleado order by  id_asistencia desc limit 1");
-            $fechaBD=$consultafecha->fetch_object()->entrada;
+            $fecha = date("Y-m-d H:i:s");
+            $id_empleado = $id->fetch_object()->id_empleado;
+
+            $consultafecha = $conn->query(" SELECT entrada  from asistencia WHERE id_empleado=$id_empleado order by  id_asistencia desc limit 1");
+            $fechaBD = $consultafecha->fetch_object()->entrada;
 //CONTRO DE FECHA
-             if (substr($fecha,0,10)==substr($fechaBD,0,10)) { ?>
-                   <script>
-                $(function notificacion() {
-                    new PNotify({
-                        title: "INCORRECTO",
-                        type: "error",
-                        text: " Ya Registraste tu entrada ",
-                        styling: "bootstrap3"
+            if (substr($fecha, 0, 10) == substr($fechaBD, 0, 10)) { ?>
+                <script>
+                    $(function notificacion() {
+                        new PNotify({
+                            title: "INCORRECTO",
+                            type: "error",
+                            text: " Ya Registraste tu entrada ",
+                            styling: "bootstrap3"
+                        })
                     })
-                })
-        </script>
-            <?php  } else {
-                 
+                </script>
+            <?php } else {
 
-                            $sql=$conn->query(" insert into asistencia(id_empleado,entrada)values($id_empleado,'$fecha') ");
 
-            if ($sql==true) { ?>
-                   <script>
-                $(function notificacion() {
-                    new PNotify({
-                        title: "CORRECTO",
-                        type: "success",
-                        text: " Correcto, Bienvenido",
-                        styling: "bootstrap3"
-                    })
-                })
-        </script>
-            <?php   } else {    ?>
-                   <script>
-                $(function notificacion() {
-                    new PNotify({
-                        title: "INCORRECTO",
-                        type: "error",
-                        text: " Error al Registrar la Asistencia ",
-                        styling: "bootstrap3"
-                    })
-                })
-        </script>
-            <?php   }
-             }
-             
-        } else {    ?>
-                   <script>
+                $sql = $conn->query(" insert into asistencia(id_empleado,entrada)values($id_empleado,'$fecha') ");
+
+                if ($sql == true) { ?>
+                    <script>
+                        $(function notificacion() {
+                            new PNotify({
+                                title: "CORRECTO",
+                                type: "success",
+                                text: " Correcto, Bienvenido",
+                                styling: "bootstrap3"
+                            })
+                        })
+                    </script>
+                <?php } else { ?>
+                    <script>
+                        $(function notificacion() {
+                            new PNotify({
+                                title: "INCORRECTO",
+                                type: "error",
+                                text: " Error al Registrar la Asistencia ",
+                                styling: "bootstrap3"
+                            })
+                        })
+                    </script>
+                <?php }
+            }
+
+        } else { ?>
+            <script>
                 $(function notificacion() {
                     new PNotify({
                         title: "INCORRECTO",
@@ -63,31 +63,31 @@ if (!empty($_POST["btnentrada"])) {
                         styling: "bootstrap3"
                     })
                 })
-        </script>
-            <?php }
-        } else {    ?>
-                   <script>
-                $(function notificacion() {
-                    new PNotify({
-                        title: "INCORRECTO",
-                        type: "error",
-                        text: " Ingrese su Cedula de Identidad ",
-                        styling: "bootstrap3"
-                    })
+            </script>
+        <?php }
+    } else { ?>
+        <script>
+            $(function notificacion() {
+                new PNotify({
+                    title: "INCORRECTO",
+                    type: "error",
+                    text: " Ingrese su Cedula de Identidad ",
+                    styling: "bootstrap3"
                 })
+            })
         </script>
-            <?php }
-    
- ?>
-<script> 
-        setTimeout(()=>{
-            window.history.replaceState(null,null,window.location.pathname);
-        }, 0);
-     </script>
+    <?php }
 
-    
-<?php
- }
+    ?>
+    <script>
+        setTimeout(() => {
+            window.history.replaceState(null, null, window.location.pathname);
+        }, 0);
+    </script>
+
+
+    <?php
+}
 ?>
 
 <!-- salida -->
@@ -95,84 +95,78 @@ if (!empty($_POST["btnentrada"])) {
 <?php
 if (!empty($_POST["btnsalida"])) {
     if (!empty($_POST["txtCI"])) {
-        $CI=$_POST["txtCI"];
-        $consulta=$conn->query( "SELECT COUNT(*) as 'total' from empleado where CI='$CI' ");
-        $id=$conn->query( "SELECT id_empleado from empleado where CI='$CI' ");
+        $CI = $_POST["txtCI"];
+        $consulta = $conn->query("SELECT COUNT(*) as 'total' from empleado where CI='$CI' ");
+        $id = $conn->query("SELECT id_empleado from empleado where CI='$CI' ");
         if ($consulta->fetch_object()->total > 0) {
-            
-            $fecha=date("Y-m-d h:i:s");
-            $id_empleado=$id->fetch_object()->id_empleado;
-            $busqueda=$conn->query(" select id_asistencia, entrada from asistencia where id_empleado=$id_empleado order by id_asistencia desc limit 1 "); 
-     // <!-- buscar la ultima entrada -->
-            // almacena id asistencia
-   //  $id_asistencia=$busqueda->fetch_object()->id_asistencia; 
-     while ($datos=$busqueda->fetch_object()) {
 
-        $id_asistencia = $datos->id_asistencia;
-        $entradaBD=$datos->entrada;
-     }
-     if (substr($fecha,0,10)!=substr($entradaBD,0,10)) { ?>
-                   <script>
-                $(function notificacion() {
-                    new PNotify({
-                        title: "INCORRECTO",
-                        type: "error",
-                        text: " Primero registra tu asistencia",
-                        styling: "bootstrap3"
+            $fecha = date("Y-m-d H:i:s");
+            $id_empleado = $id->fetch_object()->id_empleado;
+            $busqueda = $conn->query(" select id_asistencia, entrada from asistencia where id_empleado=$id_empleado order by id_asistencia desc limit 1 ");
+            while ($datos = $busqueda->fetch_object()) {
+
+                $id_asistencia = $datos->id_asistencia;
+                $entradaBD = $datos->entrada;
+            }
+            if (substr($fecha, 0, 10) != substr($entradaBD, 0, 10)) { ?>
+                <script>
+                    $(function notificacion() {
+                        new PNotify({
+                            title: "INCORRECTO",
+                            type: "error",
+                            text: " Primero registra tu asistencia",
+                            styling: "bootstrap3"
+                        })
                     })
-                })
-        </script> 
-            <?php   } else {
-         
-        $consultafecha=$conn->query(" SELECT salida  from asistencia WHERE id_empleado=$id_empleado order by  id_asistencia desc limit 1");
-            $fechaBD=$consultafecha->fetch_object()->salida;
-            if (substr($fecha,0,10)==substr($fechaBD,0,10)) { ?>
-                   <script>
-                $(function notificacion() {
-                    new PNotify({
-                        title: "INCORRECTO",
-                        type: "error",
-                        text: " Ya registro su salida",
-                        styling: "bootstrap3"
-                    })
-                })
-        </script>
-            <?php  } else {
-                     
-            $sql=$conn->query(" update asistencia set salida='$fecha' where id_asistencia=$id_asistencia ");
+                </script>
+            <?php } else {
 
-            if ($sql==true) { ?>
-                   <script>
-                $(function notificacion() {
-                    new PNotify({
-                        title: "CORRECTO",
-                        type: "success",
-                        text: " Correcto, Hasta Pronto",
-                        styling: "bootstrap3"
-                    })
-                })
-        </script>
-            <?php   } else {    ?>
-                   <script>
-                $(function notificacion() {
-                    new PNotify({
-                        title: "INCORRECTO",
-                        type: "error",
-                        text: " Error al Registrar Salida ",
-                        styling: "bootstrap3"
-                    })
-                })
-        </script>
-            <?php   }
-                 }
-     }
-     
+                $consultafecha = $conn->query(" SELECT salida  from asistencia WHERE id_empleado=$id_empleado order by  id_asistencia desc limit 1");
+                $fechaBD = $consultafecha->fetch_object()->salida;
+                if (substr($fecha, 0, 10) == substr($fechaBD, 0, 10)) { ?>
+                    <script>
+                        $(function notificacion() {
+                            new PNotify({
+                                title: "INCORRECTO",
+                                type: "error",
+                                text: " Ya registro su salida",
+                                styling: "bootstrap3"
+                            })
+                        })
+                    </script>
+                <?php } else {
+
+                    $sql = $conn->query(" update asistencia set salida='$fecha' where id_asistencia=$id_asistencia ");
+
+                    if ($sql == true) { ?>
+                        <script>
+                            $(function notificacion() {
+                                new PNotify({
+                                    title: "CORRECTO",
+                                    type: "success",
+                                    text: " Correcto, Hasta Pronto",
+                                    styling: "bootstrap3"
+                                })
+                            })
+                        </script>
+                    <?php } else { ?>
+                        <script>
+                            $(function notificacion() {
+                                new PNotify({
+                                    title: "INCORRECTO",
+                                    type: "error",
+                                    text: " Error al Registrar Salida ",
+                                    styling: "bootstrap3"
+                                })
+                            })
+                        </script>
+                    <?php }
+                }
+            }
 
 
-
-                      
-        } else {    ?>
-                   <script>
+        } else { ?>
+            <script>
                 $(function notificacion() {
                     new PNotify({
                         title: "INCORRECTO",
@@ -181,30 +175,30 @@ if (!empty($_POST["btnsalida"])) {
                         styling: "bootstrap3"
                     })
                 })
-        </script>
-            <?php }
-        } else {    ?>
-                   <script>
-                $(function notificacion() {
-                    new PNotify({
-                        title: "INCORRECTO",
-                        type: "error",
-                        text: " Ingrese su Cedula de Identidad ",
-                        styling: "bootstrap3"
-                    })
+            </script>
+        <?php }
+    } else { ?>
+        <script>
+            $(function notificacion() {
+                new PNotify({
+                    title: "INCORRECTO",
+                    type: "error",
+                    text: " Ingrese su Cedula de Identidad ",
+                    styling: "bootstrap3"
                 })
+            })
         </script>
-            <?php }
-    
- ?>
-<script> 
-        setTimeout(()=>{
-            window.history.replaceState(null,null,window.location.pathname);
-        }, 0);
-     </script>
+    <?php }
 
-    
-<?php
- }
+    ?>
+    <script>
+        setTimeout(() => {
+            window.history.replaceState(null, null, window.location.pathname);
+        }, 0);
+    </script>
+
+
+    <?php
+}
 ?>
 
